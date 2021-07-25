147eff646438c46d96a8cb069a0980fba372ad2b
an.bongiorno <an.bongiorno@reply.it>
2021-07-26 00:37:16 +0200

    add tests

--- deploy/tests.sh
+++ deploy/tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ELB_URI="http://$(grep "elb.amazonaws" ../terraform.tfstate | tr -s [[:blank:]] | cut -d':' -f2 | tr -d "\"" | tr -d "," | head -n1)"
+ELB_URI="http://$(grep "elb.amazonaws" ../terraform.tfstate | tr -d [[:blank:]] | cut -d':' -f2 | tr -d "\"" | tr -d "," | head -n1 )"
 
 echo "Inserting one doc"
-curl -X POST -H "Content-Type: application/json" --data '{"doc": {"username":"xyz","password":"xyz"}}' "${ELB_URI}/insert"
-if [[ $? -ne 0 ]]
+RC=$(curl -s -X POST -H "Content-Type: application/json" --data '{"doc": {"username":"xyz","password":"xyz"}}' "${ELB_URI}/insert" > tmp | grep -i "success" -c )
+if [[ $RC -ne 0 ]]
 then
   echo "A problem occurred while inserting the doc."
   exit 1
@@ -12,8 +12,8 @@ fi
 echo "Success!"
 
 echo "Retrieve the doc"
-curl -X POST "${ELB_URI}/find?username=xyz"
-if [[ $? -ne 0 ]]
+RC=$(curl -s -X POST "${ELB_URI}/find?username=xyz" > tmp | grep -i "success" -c)
+if [[ $RC -ne 0 ]]
 then
   echo "A problem occurred while retrieving the doc."
   exit 1
@@ -21,8 +21,8 @@ fi
 echo "Success!"
 
 echo "Updating the doc"
-curl -X PUT -H "Content-Type: application/json" --data '{"doc": {"username":"abc"}, "query":{"username": "xyz"}}' "${ELB_URI}/insert"
-if [[ $? -ne 0 ]]
+RC=$(curl -s -X PUT -H "Content-Type: application/json" --data '{"doc": {"username":"abc"}, "query":{"username": "xyz"}}' "${ELB_URI}/update" > tmp | grep -i "success" -c)
+if [[ $RC -ne 0 ]]
 then
   echo "A problem occurred while updating the doc."
   exit 1
@@ -30,8 +30,8 @@ fi
 echo "Success!"
 
 echo "Deleting the doc"
-curl -X POST -H "Content-Type: application/json" --data '{"doc": {"username":"abc"}, "query":{"username": "xyz"}}' "${ELB_URI}/insert"
-if [[ $? -ne 0 ]]
+RC=$(curl -s -X POST -H "Content-Type: application/json" --data '{"query":{"username": "xyz"}}' "${ELB_URI}/delete" > tmp| grep -i "success" -c)
+if [[ $RC -ne 0 ]]
 then
   echo "A problem occurred while updating the doc."
   exit 1
